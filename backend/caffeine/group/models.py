adf25f36c2ac883bc516a3e1bb384ac84dd09916
ihatedebug <jongh97@snu.ac.kr>
2020-10-29 18:42:06 +0900

    add group model

--- backend/caffeine/group/models.py
+++ backend/caffeine/group/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    time = models.IntegerField()  # sum of all user's daily study time
+    members = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+    def getTime(self):
+        return self.time

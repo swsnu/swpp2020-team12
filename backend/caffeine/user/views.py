d6c6eec0865e2093f537e33fa0892a240d9695ac
ihatedebug <jongh97@snu.ac.kr>
2020-10-30 17:52:47 +0900

    group backend

--- backend/caffeine/user/views.py
+++ backend/caffeine/user/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+import json
 
 # Create your views here.
+'''
+@ensure_csrf_cookie
+def token(request):
+    if request.method == 'GET':
+        return HttpResponse(status=204)
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+@csrf_exempt
+def signin(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+'''

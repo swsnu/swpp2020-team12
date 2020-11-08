ac765a699736f0f67574aa1cbe92a0e772127080
ihatedebug <jongh97@snu.ac.kr>
2020-11-08 19:41:27 +0900

    add subject frontend

--- backend/caffeine/subject/models.py
+++ backend/caffeine/subject/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 from user.models import User
-
+from datetime import timedelta
+from django import forms
 
 # Create your models here.
 
+DAYS_OF_WEEK = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+)
+
+#days를 만들거면 무조건 starttime, duration 만들어야 함
+class Days(models.Model):
+    days = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    start_time = models.TimeField(blank=True, null=False)
+    duration = models.DurationField(blank=True, null=False)
+
+
 class Subject(models.Model):
-    name = models.CharField(max_length=64)
-    time = models.DurationField()
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=64, null=False)
+    days = models.ManyToManyField(Days, null=True)
+    description = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE)
 
--- backend/caffeine/subject/tests.py
+++ backend/caffeine/subject/tests.py
@@ -1,8 +1,7 @@
 import datetime
 from django.test import TestCase
 from user.models import User
-from .models import Subject
-
+from .models import Subject, Days
 
 
 # Create your tests here.
@@ -11,12 +10,13 @@ class SubjectTestCase(TestCase):
         user1 = User.objects.create_user(username='id1', name='nickname1', password='pw1', message='message1')
         user2 = User.objects.create_user(username='id2', name='nickname2', password='pw2', message='message2')
         User.objects.create_user(username='id3', name='nickname3', password='pw3', message='message3')
-        Subject.objects.create(name='team1', description='this is description1',
-                               time=datetime.timedelta(hours=10, minutes=42), user=user1)
-        Subject.objects.create(name='team2', description='this is description2',
-                               time=datetime.timedelta(hours=15, minutes=20), user=user2)
-        Subject.objects.create(name='team3', description='this is description3',
-                               time=datetime.timedelta(hours=11, minutes=00), user=user1)
+        subject = Subject.objects.create(name='subject1', description='this is description1',
+                                         user=user1)
 
+        subject = Subject.objects.create(name='subject2', description='this is description2',
+                                         user=user2)
+
+        subject = Subject.objects.create(name='subject3', description='this is description3',
+                                         user=user1)
     def test_subject_count(self):
         self.assertEqual(Subject.objects.all().count(), 3)

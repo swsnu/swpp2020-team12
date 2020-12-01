4e30a0e61774d5a658b101d6bc9d09f479b6f197
ihatedebug <jongh97@snu.ac.kr>
2020-12-01 22:09:24 +0900

    deploy until nginx

--- backend/caffeine/caffeine/settings.py
+++ backend/caffeine/caffeine/settings.py
@@ -22,9 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=-g%30li8p%n-w26e_&j=_6_3^c-2yhm1^)8ooje)669k48sj2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CSRF_COOKIE_SECURE = True
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1', '50.16.8.104']
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
